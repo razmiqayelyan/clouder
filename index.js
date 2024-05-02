f44f941e01d5887c20d60be9f05799c6bd8c4069
razmiqayelyan <raz.miqayelyan@gmail.com>
2024-05-02 13:54:02 -0700

    new change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const path = require("path");
 const cors = require("cors");
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,20 +28,29 @@ const upload = multer({
 
 app.post('/image', upload.single('upload'), async (req, res) => {
     try {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded');
+        }
+
         await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toFile(path.join(__dirname, 'images', req.file.originalname));
         console.log('Image uploaded successfully');
         res.status(201).send('Image uploaded successfully');
     } catch (error) {
-        console.error(error);
-        res.status(400).send(error.message);
+        console.error('Error uploading image:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
 // Route to get a list of all uploaded images
 app.get('/images', (req, res) => {
-    // Serve a JSON response with a list of image file names
-    const imageFiles = fs.readdirSync(path.join(__dirname, 'images'));
-    res.json({ images: imageFiles });
+    try {
+        // Serve a JSON response with a list of image file names
+        const imageFiles = fs.readdirSync(path.join(__dirname, 'images'));
+        res.json({ images: imageFiles });
+    } catch (error) {
+        console.error('Error getting images:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
